3011be27fbe88566adf0327d412ff49651a768ab
Michael France Nelson <michaelfrancenelson@gmail.com>
2020-07-25 21:27:39 -0400

    First commit

---
+++ R/build_template_skeletons.R
@@ -0,0 +1,4 @@
+usethis::use_rmarkdown_template("moodle_schoice")
+usethis::use_rmarkdown_template("moodle_mchoice")
+usethis::use_rmarkdown_template("moodle_essay_no_upload")
+usethis::use_rmarkdown_template("moodle_essay_with_upload")
