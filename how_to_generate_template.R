08d79074a0711ca086fb0c95aa1345c06eb2dab2
Michael France Nelson <michaelfrancenelson@gmail.com>
2020-08-05 09:43:47 -0400

    adding course-specific templates

---
+++ how_to_generate_template.R
@@ -0,0 +1,51 @@
+require(usethis)
+
+
+
+# Create skeleton directory structure for a new template:
+use_rmarkdown_template(
+  template_name = "Blank Moodle Question",
+  template_description = "template for a moodle quiz question",
+  template_create_dir = FALSE
+)
+
+
+use_rmarkdown_template(
+  template_name = "Moodle single correct answer",
+  template_description = "template for a moodle single correct answer quiz question",
+  template_create_dir = FALSE
+)
+
+use_rmarkdown_template(
+  template_name = "Moodle multiple correct answer",
+  template_description = "template for a moodle multiple correct answer quiz question",
+  template_create_dir = FALSE
+)
+
+use_rmarkdown_template(
+  template_name = "Moodle numeric answer",
+  template_description = "template for a moodle numeric answer quiz question",
+  template_create_dir = FALSE
+)
+
+use_rmarkdown_template(
+  template_name = "Moodle string answer",
+  template_description = "template for a moodle string answer quiz question",
+  template_create_dir = FALSE
+)
+
+use_rmarkdown_template(
+  template_name = "Moodle image upload",
+  template_description = "template for a moodle string answer with image upload quiz question",
+  template_create_dir = FALSE
+)
+
+use_rmarkdown_template(
+  template_name = "Moodle file upload",
+  template_description = "template for a moodle string answer with any file type upload quiz question",
+  template_create_dir = FALSE
+)
+
+
+
+
